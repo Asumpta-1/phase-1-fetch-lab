887c3670d0e57b51450b957511fbf4d6dd0e0684
asumpta <sumgathoni@gmail.com>
2023-01-19 13:32:40 +0300

    Assignment

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error:', error));
+}
+
+// function renderBooks(data) {
+//   const booksContainer = document.getElementById('books');
+//   data.forEach(book => {
+//     const bookTitle = document.createElement('h3');
+//     bookTitle.innerText = book.name;
+//     booksContainer.appendChild(bookTitle);
+//   });
+// }
+
+let myJSON = { "name": "John", "age": 30 };
+
+function myFunction() {
+  console.log(myJSON);
+}
+
+myFunction();
