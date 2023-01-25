c65ef3f53f5ac5ab7453a9248b4ec599580c8416
asumpta <sumgathoni@gmail.com>
2023-01-25 19:42:06 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
   
-}
+// }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
 
-function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-    .then(response => response.json())
-    .then(data => renderBooks(data))
-    .catch(error => console.error('Error:', error));
-}
 
 // function renderBooks(data) {
 //   const booksContainer = document.getElementById('books');
@@ -32,10 +26,17 @@ function fetchBooks() {
 //   });
 // }
 
-let myJSON = { "name": "John", "age": 30 };
+// let myJSON = { "name": "John", "age": 30 };
 
-function myFunction() {
-  console.log(myJSON);
+// function myFunction() {
+//   console.log(myJSON);
+// }
+
+// myFunction();
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error:', error));
 }
-
-myFunction();
